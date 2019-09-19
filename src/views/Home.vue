2d0ea1687677101d19dc4348d849bc3788a43f26
ictk_24_邓倩红 <354964236@qq.com>
2019-09-19 16:38:50 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h2>李一一</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
